02912701d4db188a86b9c12926babb4d4e97c7f0
Shebin Rawther <shebin.rawther@gmail.com>
2014-01-15 09:00:09 +0000

    moving code from old project to new one

---
+++ app/controllers/index.js
@@ -0,0 +1,35 @@
+function doClick(e) {
+    alert($.label.text);
+}
+
+$.index.open();
+
+if (Ti.Platform.osname === 'iphone' || Ti.Platform.osname === 'ipad')
+{
+  var touchTestModule = undefined;
+  try
+  {
+    touchTestModule = require("com.soasta.touchtest");
+  }
+  catch (tt_exception)
+  {
+    Ti.API.error("com.soasta.touchest module is required");
+  }
+
+  var cloudTestURL = Ti.App.getArguments().url;
+  if (cloudTestURL != null)
+  {
+    // The URL will be null if we don't launch through TouchTest.
+    touchTestModule && touchTestModule.initTouchTest(cloudTestURL);
+  }
+
+  Ti.App.addEventListener('resumed',function(e)
+  {
+    // Hook the resumed from background
+    var cloudTestURL = Ti.App.getArguments().url;
+    if (cloudTestURL != null)
+    {
+      touchTestModule && touchTestModule.initTouchTest(cloudTestURL);
+    }
+  });
+}
