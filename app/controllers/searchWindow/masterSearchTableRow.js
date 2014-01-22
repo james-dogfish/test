d48d66616a9ac3232d726cb796cb62d72e1f332b
Andy Mills <andy@Andys-MacBook-Pro.local>
2014-01-22 13:48:07 +0000

    added crossing type to search window

--- app/controllers/searchWindow/masterSearchTableRow.js
+++ app/controllers/searchWindow/masterSearchTableRow.js
@@ -4,3 +4,6 @@ $.crossingRow.filter = crossingObject.name;
 
 $.crossingName.text = "Crossing Name - " + crossingObject.name;
 $.crossingElr.text = "Crossing ID - "+ crossingObject.id;
+
+$.crossingType.text = "Crossing Type - "+ crossingObject.type;
+
