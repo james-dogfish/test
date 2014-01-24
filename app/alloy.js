1fb379d0653aa0678654b48f42754a8c85fa3ed5
Shebin Rawther <shebin.rawther@gmail.com>
2014-01-24 14:46:55 +0000

    touchtest crap

--- app/alloy.js
+++ app/alloy.js
@@ -11,3 +11,4 @@ Ti.API.info("com.appcelerator.apm module is not available");
 // Initialize the module if it is defined
 Alloy.Globals.apm && Alloy.Globals.apm.init();
 // END: APM code injection
+// Touchtest params from old projectif (Ti.Platform.osname === 'iphone' || Ti.Platform.osname === 'ipad') {	var touchTestModule = undefined;	try {		touchTestModule = require("com.soasta.touchtest");	} catch (tt_exception) {		Ti.API.error("com.soasta.touchest module is required");	}	var cloudTestURL = Ti.App.getArguments().url;	if (cloudTestURL != null) {		// The URL will be null if we don't launch through TouchTest.		touchTestModule && touchTestModule.initTouchTest(cloudTestURL);	}	Ti.App.addEventListener('resumed', function(e) {		// Hook the resumed from background		var cloudTestURL = Ti.App.getArguments().url;		if (cloudTestURL != null) {			touchTestModule && touchTestModule.initTouchTest(cloudTestURL);		}	});}
