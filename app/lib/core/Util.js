48497fe82ab7d4f112717c12c1ddab3f9f2a0723
Shebin Rawther <shebin.rawther@gmail.com>
2014-02-01 21:32:51 +0000

    ios6 changes, my risk assessments window is now fine in ios6, added ios7 detection function in util

--- app/lib/core/Util.js
+++ app/lib/core/Util.js
@@ -7,6 +7,20 @@ function _Util() {
         routeFiles,
         Alloy = require("alloy");
     //downloader = require('tools/downloader');
+    
+    self.isIOS7Plus = function() {
+        // iOS-specific test
+        if (Titanium.Platform.name === 'iPhone OS') {
+            var version = Titanium.Platform.version.split(".");
+            var major = parseInt(version[0], 10);
+
+            // Can only test this support on a 3.2+ device
+            if (major >= 7) {
+                return true;
+            }
+        }
+        return false;
+    };
 
     self.getCmsUrl = function () {
         return cmsUrl;
