5cf7f4cc7bdb41c33cb829adc5577f8593494e2d
James  Harbord <james.harbord@me.com>
2017-01-31 20:18:14 +0000

    build.sh created online with Bitbucket

---
+++ releasescripts/build.sh
@@ -0,0 +1,7 @@
+var fs = require('fs'),
+	configJson = require('../app/config');
+
+configJson.global.currentEnv = $BUILD_ENV;
+fs.writeFileSync('./app/config.json', JSON.stringify(configJson, null, 4), 'utf8');
+
+console.log('Making uat changes to config.json');
