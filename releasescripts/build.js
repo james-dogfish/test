96c7a6c308d89a21333554c4948c86ba35aae666
James  Harbord <james.harbord@me.com>
2017-02-01 10:04:49 +0000

    build.js edited online with Bitbucket

--- releasescripts/build.js
+++ releasescripts/build.js
@@ -1,35 +1,14 @@
-// Will do the following
-// Change server url id in config.json to the Prod one
+// Will do the following 
+// Change google analytics id in config.json to the production one
 // as we are generating a prod build all the time from jenkins and hence config.json
 // values need to be overridden
 
 var fs = require('fs'),
-	configJson = require('../app/config');
-
-configJson.global.serverUrl = 'http://server.iconsolutions.com/alcrm3/';
-//configJson.global.headerCol = '#f7f7f7';
+	configJson = require('../app/config'),
+	//tiapp = require('tiapp.xml').load('./tiapp.xml');
 
 
-if (process.argv[2] === "dte") {
-	console.log("change config to dte");
-	configJson.global.currentEnv.serverUrl = 'https://icondte.networkrail.co.uk/WebServices/';
-	
-else if (process.argv[2] === "ppte") {
-console.log("change config to ppte");
-configJson.global.currentEnv.serverUrl = 'https://iconppte.networkrail.co.uk/WebServices';
-
-} else(process.argv[2] === "fat") {
-	console.log("change config to fat");
-	configJson.global.currentEnv.serverUrl = 'http://server.iconsolutions.com/alcrm3/';
-}
-
+configJson.global.currentEnv = '$BUILD_ENV';
 
 fs.writeFileSync('./app/config.json', JSON.stringify(configJson, null, 4), 'utf8');
 
-console.log('Making prod changes to config.json');
-
-
-
-function copyFile(fromFile, toFile) {
-	fs.createReadStream(fromFile).pipe(fs.createWriteStream(toFile));
-}
