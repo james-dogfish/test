59ba1ee1b03929ae903a929cc3152806a92c80cf
Andy-dogfish <andy@dogfi.sh>
2017-02-07 09:58:28 +0000

    fix builds.js

--- releasescripts/build.js
+++ releasescripts/build.js
@@ -9,26 +9,26 @@ var fs = require('fs'),
 if (process.argv[2] === "fat") {
 	console.log("change config to fat");
 	configJson.global.currentEnv = 'fat';
-	configJson.global.serverUrl = 'http://server.iconsolutions.com/alcrm3/';
-} 
+	configJson.global.serverUrl.fat = 'http://server.iconsolutions.com/alcrm3/';
+}
 
 else if (process.argv[2] === "ppte") {
 	console.log("change config to ppte");
 	configJson.global.currentEnv = 'ppte';
-	configJson.global.serverUrl = 'https://iconppte.networkrail.co.uk/WebServices/';
-} 
+	configJson.global.serverUrl.ppte = 'https://iconppte.networkrail.co.uk/WebServices/';
+}
 
 else if (process.argv[2] === "dte") {
 	console.log("change config to dte");
 	configJson.global.currentEnv = 'dte';
-	configJson.global.serverUrl = 'https://icondte.networkrail.co.uk/WebServices/';
-	
+	configJson.global.serverUrl.dte = 'https://icondte.networkrail.co.uk/WebServices/';
+
 }
 
 else {
 	console.log("change config to prod");
 	configJson.global.currentEnv = 'prod';
-	configJson.global.serverUrl = 'https://iconprod.networkrail.co.uk/WebServices/';
+	configJson.global.serverUrl.prod = 'https://iconprod.networkrail.co.uk/WebServices/';
 
 }
 
